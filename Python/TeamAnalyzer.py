18341a26239a0440e10d38650da7ca0273c3c3db
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-05-03 17:05:35 -0700

    Update TeamAnalyzer.py

--- Python/TeamAnalyzer.py
+++ Python/TeamAnalyzer.py
@@ -6,29 +6,52 @@ types = ["bug","dark","dragon","electric","fairy","fight",
     "fire","flying","ghost","grass","ground","ice","normal",
     "poison","psychic","rock","steel","water"]
 
-# Take six parameters on the command-line
 if len(sys.argv) < 6:
     print("You must give me six Pokemon to analyze!")
     sys.exit()
 
 team = []
+
 for i, arg in enumerate(sys.argv):
     if i == 0:
         continue
 
-    # Analyze the pokemon whose pokedex_number is in "arg"
+    pokemon_id = int(arg)
+    connection = sqlite3.connect("pokemon.sqlite")
+    cursor = connection.cursor()
 
-    # You will need to write the SQL, extract the results, and compare
-    # Remember to look at those "against_NNN" column values; greater than 1
-    # means the Pokemon is strong against that type, and less than 1 means
-    # the Pokemon is weak against that type
+    cursor.execute("SELECT * FROM imported_pokemon_data WHERE pokedex_number=?", (pokemon_id,))
+    pokemon = cursor.fetchone()
+
+    name = pokemon[1]
+    type1 = types[int(pokemon[2])-1]
+    type2 = types[int(float(pokemon[18])-1)] if pokemon[18] != '' else ''
+    strengths = []
+    weaknesses = []
+    for j, t in enumerate(types):
+        if pokemon[3 + j] != "":
+            if float(pokemon[3 + j]) > 1:
+                strengths.append(t)
+            elif float(pokemon[3 + j]) < 1:
+                weaknesses.append(t)
+
+    print(f"Analyzing {i}\n{name} ({type1} {type2}) is strong against {strengths} but weak against {weaknesses}")
+    connection.close()
+
 
 answer = input("Would you like to save this team? (Y)es or (N)o: ")
 if answer.upper() == "Y" or answer.upper() == "YES":
     teamName = input("Enter the team name: ")
 
     # Write the pokemon team to the "teams" table
+    conn = sqlite3.connect('pokemon.sqlite')
+    cursor = conn.cursor()
+    if len(team) == 6:
+        cursor.execute('INSERT INTO teams (team_name, pokemon_1, pokemon_2, pokemon_3, pokemon_4, pokemon_5, pokemon_6) VALUES (?, ?, ?, ?, ?, ?, ?)', (teamName, team[0], team[1], team[2], team[3], team[4], team[5]))
+    else:
+        print("Error: Team does not have exactly 6 Pokemon.")
+    conn.commit()
+
     print("Saving " + teamName + " ...")
 else:
     print("Bye for now!")
-
